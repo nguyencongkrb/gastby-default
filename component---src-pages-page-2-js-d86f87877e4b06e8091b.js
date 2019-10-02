fb1280fedce5ceeaf7cf2990ad4c128f004b178d
nguyencongkrb <nguyencongkrb@gmail.com>
2019-10-02 22:58:09 +0700

    Updates

---
+++ component---src-pages-page-2-js-d86f87877e4b06e8091b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{221:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(74),c=a(219),r=a(218);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-d86f87877e4b06e8091b.js.map
